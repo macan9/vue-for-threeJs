1b614553011d583733c3085b0459332d6833e970
macan9 <729672251@qq.com>
2023-09-22 22:05:14 +0800

    fixed

--- src/views/blogSystem/BlogMain.vue
+++ src/views/blogSystem/BlogMain.vue
@@ -1,8 +1,22 @@
 <template>
-    <div>
+    <div class="BlogMain">
         <div>梦里回到他的身旁</div>
         <div>密语中风不在凉</div>
         <div>永远都像初次见你那样</div>
         <div>使我心荡漾</div>
     </div>
 </template>
+<style lang="scss">
+.BlogMain{
+    padding: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    font-size: 30px;
+    >div{
+        width: 100%;
+        margin: 20px;
+    }
+}
+</style>
