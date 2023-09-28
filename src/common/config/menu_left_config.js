26fe8d279500a96d05d3ee2b7dd8f3d29cd9f2e9
macan9 <729672251@qq.com>
2023-09-28 16:40:45 +0800

    active menu

---
+++ src/common/config/menu_left_config.js
@@ -0,0 +1,87 @@
+export const menu_left_config = [
+    {
+        pid: 1,
+        children: [
+            {
+                label: "我的主页",
+                value: 1,
+                path: "/BlogMain",
+                icon: "House",
+                children:[
+                    {
+                        label: "用户管理",
+                        value: 2,
+                        path: "/UserEdit",
+                        icon: "House"
+                    },
+                ]
+            },{
+                label: "用户管理",
+                value: 2,
+                path: "/UserEdit",
+                icon: "House"
+            },
+        ] 
+    },{
+        pid: 2,
+        children: [
+            {
+                label: "我的主页",
+                value: 1,
+                path: "/BlogMain",
+                icon: "House"
+            }
+        ] 
+    },{
+        pid: 3,
+        children: [
+            {
+                label: "用户管理",
+                value: 2,
+                path: "/UserEdit",
+                icon: "House"
+            },
+        ] 
+    },{
+        pid: 4,
+        children: [
+            {
+                label: "我的主页",
+                value: 1,
+                path: "/BlogMain",
+                icon: "House"
+            }
+        ] 
+    },{
+        pid: 5,
+        children: [
+            {
+                label: "用户管理",
+                value: 2,
+                path: "/UserEdit",
+                icon: "House"
+            },
+        ] 
+    },{
+        pid: 6,
+        children: [
+            {
+                label: "我的主页",
+                value: 1,
+                path: "/BlogMain",
+                icon: "House"
+            }
+        ] 
+    },{
+        pid: 7,
+        children: [
+            {
+                label: "用户管理",
+                value: 2,
+                path: "/UserEdit",
+                icon: "House"
+            },
+        ] 
+    }
+]
+    
---
+++ src/common/config/menu_top_config.js
@@ -0,0 +1,39 @@
+export const menu_top_config = [
+    {
+        label: "用户首页",
+        value: 1,
+        path: "/BlogMain",
+        icon: "House"
+    },{
+        label: "二维地图",
+        value: 2,
+        path: "/BlogMain",
+        icon: "Location"
+    },{
+        label: "三维场景",
+        value: 3,
+        path: "/BlogMain",
+        icon: "OfficeBuilding"
+    },{
+        label: "博客管理",
+        value: 4,
+        path: "/BlogMain",
+        icon: "OfficeBuilding"
+    },{
+        label: "简历展示",
+        value: 5,
+        path: "/BlogMain",
+        icon: "List"
+    },{
+        label: "聊天模块",
+        value: 6,
+        path: "/BlogMain",
+        icon: "Iphone"
+    },{
+        label: "趣味功能",
+        value: 7,
+        path: "/BlogMain",
+        icon: "WindPower"
+    },
+]
+
