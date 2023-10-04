682ece47dc64a90eed5dd7c4c2f465f5fa93d49b
macan9 <729672251@qq.com>
2023-10-05 00:54:12 +0800

    map markers

--- src/views/leafletMap/LeafletMap.vue
+++ src/views/leafletMap/LeafletMap.vue
@@ -8,24 +8,21 @@
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import { onMounted } from 'vue';
+import { mapMarkers } from '@/common/mock/mapMarkers';
  
 export default{
     
     setup(){
         
         onMounted(() =>{
-            // 加载底图,需要基于真实节点 #map 来渲染
-            const map = L.map('map').setView([31.2304, 121.4737], 11)
 
-            // 此处可以引用不同的瓦片生成不同的底图，例如腾讯地图、高德地图或者其他瓦片供应商等
-            //其中 {x}、{y} 和 {z} 分别表示瓦片的行列号和缩放级别，lang=zh_cn 表示地图语言为中文，
-            // 尺寸为 1x1，style=8 表示显示路网地图。
-            L.tileLayer('https://webst04.is.autonavi.com/appmaptile?style=7&x={x}&y={y}&z={z}', {
-                attribution: '© 高德地图'
-            }).addTo(map)
+            // 初始化底图
+            const map = initMap()
+            
+            // 挂载多种标注，及标注事件
+            addMarkers(map)
 
-             // 挂载标注点、园、三角形 非瓦片图层
-            // const marker = L.marker([31.2304 ,121.47]).addTo(map)
+            // 园、三角形 非瓦片图层
             const circle = L.circle([31.23, 121.47], {
                 color: 'red',
                 fillColor: '#f03',
@@ -37,17 +34,17 @@ export default{
                 [31.51, 121.41],
                 [31.30, 121.68]
             ]).addTo(map)
+            // 添加弹窗事件
+            circle.bindPopup("I am a circle.")
+            polygon.bindPopup("I am a polygon.")
 
-            // 默认弹窗
+
+            // 默认地图弹窗
             const popup = L.popup()  
                 .setLatLng([31.23, 121.34])
                 .setContent("I am a standalone popup.")
                 .openOn(map);
-            // 添加弹窗事件
-            // marker.bindPopup("<b>Hello world!</b><br>I am a popup.")
-            circle.bindPopup("I am a circle.")
-            polygon.bindPopup("I am a polygon.")
-
+            
             // 添加底图点击事件
             const onMapClick = (e) =>{
                 popup
@@ -55,7 +52,6 @@ export default{
                 .setContent("You clicked the map at " + e.latlng.toString())
                 .openOn(map);
             }
-
             map.on('click', onMapClick);
 
             // 获取并渲染覆盖在底图上的图层
@@ -82,8 +78,32 @@ export default{
 
 
         })
-        
-      
+          
+        const initMap = () => {
+            // 加载底图,需要基于真实节点 #map 来渲染
+            const map = L.map('map').setView([31.2304, 121.4737], 11)
+
+            // 此处可以引用不同的瓦片生成不同的底图，例如腾讯地图、高德地图或者其他瓦片供应商等
+            //其中 {x}、{y} 和 {z} 分别表示瓦片的行列号和缩放级别，lang=zh_cn 表示地图语言为中文，
+            // 尺寸为 1x1，style=8 表示显示路网地图。
+            L.tileLayer('https://webst04.is.autonavi.com/appmaptile?style=7&x={x}&y={y}&z={z}', {
+                attribution: '© 高德地图'
+            }).addTo(map)
+            return map
+        }
+
+        const addMarkers = (map) => {
+            mapMarkers.map(i => {
+                const myIcon = L.icon({
+                    iconUrl: `/img/map_icon/${i.icon}`,  // 图标文件路径,这里用了public的绝对路径
+                    iconSize: i.size   // 图标大小
+                }) 
+                i.position.map(j=>{
+                    const marker = L.marker(j, {icon: myIcon}).addTo(map)
+                    marker.bindPopup(`<b>简介</b><br>我是${i.label}`)
+                })
+            })  
+        }
     },
 }
 </script>
